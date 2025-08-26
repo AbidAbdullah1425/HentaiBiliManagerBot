306261b22038106cca30d1a636fd93a7ea6782df
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-08-26 09:59:40 +0600

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -5,14 +5,17 @@ import os
 
 # send the file to db channel
 async def upload(Bot: Client, filepath, caption, message):
-    if not os.path.exists(filepath):
-        await message.reply_message(f"File NOT found! {filepath}")
-        raise FileNotFoundError(f"File NOT Found {filepath}")
+    try:
+      if not os.path.exists(filepath):
+          await message.reply_text(f"File NOT found! {filepath}")
+          raise FileNotFoundError(f"File NOT Found {filepath}")
     
-    send_vid = await Bot.send_video(
-        chat_id=DB_CHANNEL_ID,
-        video=filepath,
-        caption=caption,
-        parse_mode="HTML"
-    )
-    return send_vid
+      send_vid = await Bot.send_video(
+          chat_id=DB_CHANNEL_ID,
+          video=filepath,
+          caption=caption,
+          parse_mode="HTML"
+      )
+      return True, send_vid
+    except Exception as e:
+        return False, str(e)
