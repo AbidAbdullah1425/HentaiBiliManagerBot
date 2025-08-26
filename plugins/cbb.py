71668e618dde704595a5f81d8d445aa14c54546a
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-08-26 19:26:48 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,6 +2,7 @@ from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.enums import ParseMode
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
@@ -14,7 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 "<b>○ Wᴇʙsɪᴛᴇ:</b> <a href='https://onlynoco.vercel.app'>Pᴏʀᴛғᴏʟɪᴏ</a>\n"
                 "<b>○ Dᴏɴᴀᴛᴇ:</b> <a href='https://onlynoco.vercel.app/donate'>Sᴜᴘᴘᴏʀᴛ</a>"
             ).format(OWNER_ID=OWNER_ID),
-            parse_mode="html",
+            parse_mode=ParseMode.HTML,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("🔒 Close", callback_data="close")]]
