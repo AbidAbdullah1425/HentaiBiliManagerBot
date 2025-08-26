0ff039a80351774e64dd5b1d38b9dcce347d0bf2
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-08-26 22:24:12 +0600

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -131,12 +131,13 @@ async def post_command(client, message):
    
     # here main channel post logic 
     try:
-        await client.send_photo(
-          chat_id=POST_CHANNEL_ID,
-          photo=thumbnail_path,
-          caption=None,
-          reply_markup=buttons
-        )
+        with open(thumbnail_path, "rb") as thumb:
+            await client.send_photo(
+              chat_id=POST_CHANNEL_ID,
+              photo=thumbnail_path,
+              caption=None,
+              reply_markup=buttons
+            )
     except Exception as e:
       await message.reply_text(f"Send Post Failed! {str(e)}")
    
